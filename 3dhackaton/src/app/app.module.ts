ee12fb1fc9bcf6efb15c54d903b6b1d833a2bc27
rckam <rckmiri@gmail.com>
2022-03-10 09:19:35 +0100

    added component main and leftmenu

--- 3dhackaton/src/app/app.module.ts
+++ 3dhackaton/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { MainDisplayComponent } from './main-display/main-display.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent,
+    MainDisplayComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    NgbModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
