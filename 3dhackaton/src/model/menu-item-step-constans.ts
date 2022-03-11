10998cba5cf279c398bd72123e4a594b83930de4
rckam <rckmiri@gmail.com>
2022-03-11 08:54:45 +0100

    fixes

--- 3dhackaton/src/model/menu-item-step-constans.ts
+++ 3dhackaton/src/model/menu-item-step-constans.ts
@@ -2,9 +2,9 @@ import {MenuItemStep} from "./menu-item-step";
 
 export class MenuItemStepConstants {
   public static UNBOLT = new MenuItemStep(1, "Enlever les boulons", "Utiliser la clé" +
-    "N°4 pour désserer les boulons de la roue avant", 4.7);
+    " N°4 pour désserer les boulons de la roue avant", 4.7);
   public static REMOVE_WHEEL = new MenuItemStep(2, "Enlever la roue", "Attraper la roue avec vos deux main (la gauche et la droite) " +
-    "puis tirer sur la roue avant du scooter afin de la retirer", 4.7);
+    " puis tirer sur la roue avant du scooter afin de la retirer", 4.7);
   public static END_STEP = new MenuItemStep(3, "Fin", "Vous venez de réussir l'ensemble des étapes, bien joué 😎", 4.7);
 
   public static WHEEL_REMOVAL = [
